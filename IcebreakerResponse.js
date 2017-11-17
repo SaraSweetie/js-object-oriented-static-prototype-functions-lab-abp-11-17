7b4f63351288688f3c8819edf7a3894f6611fe85
Sara Smith <sarasmith887@gmail.com>
2017-11-17 22:21:24 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,7 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(emails){
+   return new IcebreakerResponse(email)
+  })​
